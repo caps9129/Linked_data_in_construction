da67f1b15c06f6a0f3508ba07e0aa5604b4920ab
DESKTOP-DD0AJV1\caps9 <caps9129@gmail.com>
2018-02-28 16:22:10 +0800

    1.改變檔案的紀錄方式，避免讀取到錯誤的字眼 2.成功跑完取得頁面

--- Construction_Engineering_resume/auto_login.php
+++ Construction_Engineering_resume/auto_login.php
@@ -1,5 +1,6 @@
 <?php
-
+ini_set('max_execution_time', '0');
+ini_set("memory_limit","8192M");
 //初始化變量
 
 header("Content-Type:text/html; charset=utf-8");
@@ -7,40 +8,66 @@ $cookie_file = "valid.tmp";
 $login_url = "http://cpabm.cpami.gov.tw/cers/SearchLicList.do";    
 $verify_code_url = "http://cpabm.cpami.gov.tw/cers/img_code.jsp";    //取得驗證碼圖片
 $file = 'code.txt';    
-$timeout = 5;   //設置等待時間
-/*$erase = "";*/
+$timeout = 10;   //設置等待時間
+$data_Digits = 4; 
+$page = 1;  //傳入與取得頁數(設為"1"是為了在迴圈跑動第一次，以取得正確的page頁數)
+//$yearIndex = 0;
+$pages_year = array(); //[year][pages]
+
 
 #define city->citycode
 $countycode = array();
 $countycode = array("台北市"=>"G00", "高雄市"=>"H00", "基隆市"=>"I10","宜蘭縣"=>"I20", "新北市"=>"I30", "桃園市"=>"I40", "新竹市"=>"I50",  
                   "新竹縣"=>"I60", "苗栗縣"=>"I70", "台中市"=>"I80", "彰化縣"=>"IA0","南投縣"=>"IB0", "雲林縣"=>"IC0", "嘉義市"=>"ID0",
                   "嘉義縣"=>"IE0", "台南市"=>"IF0", "屏東縣"=>"II0", "花蓮縣"=>"IJ0","台東縣"=>"IK0", "澎湖縣"=>"IL0", "連江縣"=>"J10",
-                  "金門縣"=>"J20");
+"金門縣"=>"J20");
+$year = array("0"=>"100", "1"=>"101", "2"=>"102", "3"=>"103", "04"=>"104", "05"=>"105","06"=>"106", "07"=>"107");
 
-/*foreach($countycode as $conutyKey => $countyValue){
-    echo $conutyKey;
-    $fp = create_txt($conutyKey);
-
-}*/
 /**************************************main*************************************************/
+$fp = create_txt($countycode);
 $cookie_file = getCookie($verify_code_url, $cookie_file, $timeout);
 $code = getCheckNumber($verify_code_url, $cookie_file, $file);
-file_put_contents($file ,"");
-$post = http_build_query(array("budare" => "G00", "license_yy" => "100", "license_no1" => "", "insrand" => $code, "submit" => '%ACd%B8%DF'));
-$html = post($login_url, $post, $cookie_file);
 
-//BIG5 to UTF8。加上IGNORE以忽略非法字眼
-$html = iconv("Big5", "UTF-8//IGNORE", $html); 
-$xpath = create_dom($html);
-$page = getpage($xpath);
+ //運作模式為取得先取得同一縣市同一年度不同頁數 >> 取得同一縣市不同年度不同頁數 >> 取得不同縣市不同年度不同頁數
+foreach($countycode as $countycodeKey => $countycodeValue){  //跑縣市
+    
+    foreach($year as $yearKey => $yearValue){   //跑年度
 
+        for($i = 1 ; $i <= $page ; $i++){   //跑頁數
+            $post = http_build_query(array("d-16544-p" => "$i", "budare" => $countycodeValue, "license_yy" => $yearValue, "license_no1" => "", "insrand" => $code, "submit" => '%ACd%B8%DF'));
+            /*echo "post:".$post."<br>";*/
+            $html = post($login_url, $post, $cookie_file);
+            
+            $html = iconv("Big5", "UTF-8//IGNORE", $html); //BIG5 to UTF8。加上IGNORE以忽略非法字眼
+            echo $countycodeKey."<br>";
+            $xpath = create_dom($html);
+            if($i == 1){    //只要第一次拿到頁數就好了~~
+                $page = getpage($xpath, $data_Digits, $page);
+            }   
+        }
+        
+        
+        
+    }
+    
+}
+if(unlink($file))
+    echo "成功刪除!!";
+else
+    echo "刪除失敗!!";
+print_r($pages_year);
 
 exit;
 
-//登錄需要驗證的參數，根據登錄的網站要求而定
 
 /*********************************************************************************************/
 
+
+function getContent($xpath){
+    foreach($xpath->query('//span[@class = "pagebanner"]') as $node){
+        
+    }
+}
 
 //取得session
 function getCookie($cookie_url, $cookie_file, $timeout){
@@ -87,11 +114,13 @@ function getCheckNumber($image_url, $cookie_file, $file){
     fwrite($fp, $data);
     fclose($fp);
     //初始化
+    $code = FALSE;
     $code = "";    
+    fopen("{$file}", "w");
+    var_dump((bool) $code); 
     do{
         $code = file_get_contents($file);
     }while($code == FALSE);
- 
     return $code;
 }
 
@@ -100,20 +129,31 @@ function getCheckNumber($image_url, $cookie_file, $file){
 
 
 //取得頁數
-function getpage($xpath){
+function getpage($xpath, $data_Digits, $page){
+    /*if($xpath){*/
     foreach($xpath->query('//span[@class = "pagebanner"]') as $node) {  //取得頁數
-        $temp = array();
+        $temp = array();    //儲存所有可能字元
+        $temp_1 = array();  //儲存正確字元
         $temp = str_split($node->textContent, 1); //分割成1個字元存入陣列
         $temp = array_splice($temp, 17 ,19);/**/ 
-        $temp = array_splice($temp, 20);/**/
-        print_r($temp);
-        
+        $temp = array_splice($temp, 0, $data_Digits);/*取4位數，之後筆數可以往後取*/
+        foreach($temp as $tempKey => $tempValue){
+            if(is_numeric($tempValue)){ //判斷是否為數字
+                array_push($temp_1, $tempValue);
+            }
+        }  
+        $page =  implode("", $temp_1);
+        $page = ceil($page / 10.0); //每一頁10筆
+        return $page;
     }
 }
 
 //創建文檔，傳入各縣市
-function create_txt($cKey){
-    $fp=fopen("{$cKey}.txt","w");
+function create_txt($countycode){
+    foreach($countycode as $conutyKey => $countyValue){
+        $fp=fopen("{$conutyKey}.txt","w");
+        fprintf($fp,"建造執照,建築地點,起造人,設計人,監造人,承造人\n");
+    }
     return $fp;
 }
 
@@ -126,66 +166,6 @@ function create_dom($html){
     return $xpath;
 }
 
-
-
-
-/*foreach ($xpath->query('//table/tr[@class = "list0"]') as $node) {   //取table,tr.class = list0之列
-    
-    if($xpath->query('td/img', $node)) {                             //判斷每一列開頭是否有圖片
-        foreach ($xpath->query('td/img/@src', $node) as $cell) {     //取出圖片網址
-            $rowcleaned = $cell->textContent;
-            if($rowcleaned == "/images/notation/hand.gif")           //判斷何種圖片
-                echo "註銷";
-            else
-                echo "失效";
-        }
-        foreach ($xpath->query('td', $node) as $cell) {              //印出有圖片列之相關資訊
-            $rowcleaned = $cell->textContent;
-            echo $rowcleaned . "<br />";
-        }
-        echo "<br />";
-        echo "<br />";
-    } 
-    
-    else {                                                            //沒有圖片的列
-        foreach ($xpath->query('td', $node) as $cell) {
-            $rowcleaned = $cell->textContent;
-            echo $rowcleaned . "<br />";
-            
-        }
-        echo "<br />";
-        echo "<br />";
-    }   
-}
-
-foreach ($xpath->query('//table/tr[@class = "list1"]') as $node) {   
-    
-    if($xpath->query('td/img', $node)) {
-        foreach ($xpath->query('td/img/@src', $node) as $cell) {
-            $rowcleaned = $cell->textContent;
-            if($rowcleaned == "/images/notation/hand.gif")
-                echo "註銷";
-            else
-                echo "失效";
-        }
-        foreach ($xpath->query('td', $node) as $cell) {
-            $rowcleaned = $cell->textContent;
-            echo $rowcleaned . "<br />";
-        }
-        echo "<br />";
-        echo "<br />";
-    } 
-    
-    else {
-        foreach ($xpath->query('td', $node) as $cell) {
-            $rowcleaned = $cell->textContent;
-            echo $rowcleaned . "<br />";
-            
-        }
-        echo "<br />";
-        echo "<br />";
-    }   
-}*/
   
   
 ?>
