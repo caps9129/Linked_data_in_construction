5486b303525d8592d581e7e30290145ca1d0d5a0
DESKTOP-DD0AJV1\caps9 <caps9129@gmail.com>
2018-03-20 08:58:51 +0800

    Organize and clear

--- Construction_Engineering_resume/dbConnect.php
+++
@@ -1,9 +0,0 @@
-<?php
- $db = mysqli_connect("db.sgis.tw", "sinicaintern", "27857108311", "building");
- if(!$db){
-     die("無法對資料連線". mysqli_connect_error());
- }
- else
-     echo "Successful!!";
-
-?>
