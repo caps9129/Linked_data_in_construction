2e125e81afb8148a568a190dd0700f6585e41407
DESKTOP-DD0AJV1\caps9 <caps9129@gmail.com>
2018-04-17 10:21:17 +0800

    針對4個建築table建立xml

--- create_xml/create_xml.php
+++ create_xml/create_xml.php
@@ -1,91 +1,201 @@
 <?php
-    /**
-     * Mapping an RDF class type to a PHP Class
-     *
-     * This example fetches and displays artist information from the
-     * BBC Music website. The artist object is an instance of the
-     * Model_MusicArtist class, so it is possible to call custom PHP
-     * methods on the object.
-     *
-     * It also demonstrates setting new namespaces.
-     *
-     * @package    EasyRdf
-     * @copyright  Copyright (c) 2009-2013 Nicholas J Humfrey
-     * @license    http://unlicense.org/
-     */
 
-    set_include_path(get_include_path() . PATH_SEPARATOR . '../lib/');
-    require_once "EasyRdf.php";
-    require_once "html_tag_helpers.php";
+    define("DB_HOST", "db.sgis.tw");
+    define("DB_USER", "sinicaintern");
+    define("DB_PASS", "27857108311");
+    define("DB_NAME", "building");
 
+    $DB_Table_Information = "architect_information";
+    $DB_Table_Office = "architect_office";
+    $DB_Table_Project = "architect_project";
+    $DB_Table_Contractor = "building_contractor";
 
-    class Model_MusicArtist extends EasyRdf_Resource
-    {
-        function birthEvent()
-        {
-            foreach ($this->all('bio:event') as $event) {
-                if (in_array('bio:Birth', $event->types())) {
-                    return $event;
+    do{
+        printf("1.create architect information xml\n");
+        printf("2.create architect office xml\n");
+        printf("3.create architect project xml\n");
+        printf("4.create building contractor xml\n");
+        printf("5.create all xml\n");
+        printf("0.EXIT\n");
+        $choice = read_chioce();
+        if($choice == 1){
+            $DataBase = new DBClass();
+            $DataBase->select_DB($DB_Table_Information);
+            $DataBase->disconnect();
+        }
+        else if($choice == 2){
+            $DataBase = new DBClass();
+            $DataBase->select_DB($DB_Table_Office);
+            $DataBase->disconnect();
+        }
+        else if($choice == 3){
+            $DataBase = new DBClass();
+            $DataBase->select_DB($DB_Table_Project);
+            $DataBase->disconnect();
+        }
+        else if($choice == 4){
+            $DataBase = new DBClass();
+            $DataBase->select_DB($DB_Table_Contractor);
+            $DataBase->disconnect();
+        }
+        else if($choice == 5){
+            $DataBase = new DBClass();
+            $DataBase->select_DB($DB_Table_Information);
+            $DataBase->select_DB($DB_Table_Office);
+            $DataBase->select_DB($DB_Table_Project);
+            $DataBase->select_DB($DB_Table_Contractor);
+            $DataBase->disconnect();
+        }
+    }while($choice != 0);
+    
+    exit;
+        
+
+    class DBClass {
+
+        var $conn, $query, $result;
+      
+        
+        public function __construct() {
+            $this->connect();
+        }
+    
+        public function disconnect() {
+            mysqli_close($this->conn);
+        }
+    
+        public function reconnect() {
+            $this->disconnect();
+            $this->connect();
+        }
+    
+        public function connect() {
+            $this->conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+            
+            if(!$this->conn){
+                die("dbConnect fail". mysqli_connect_error()."\n");
+                exit;
+            }
+            else{
+                echo "dbConnect Successful!!<br>\n";
+                if (!$this->conn->set_charset("utf8")) {
+                    printf("Error loading character set utf8: %s\n", $this->conn->error);
+                    
+                } else {
+                    printf("Current character set: %s\n", $this->conn->character_set_name());
+                }  
+            }
+        }
+
+        public function create_xml($DB_Table_Option, $data, $colnames) {
+            
+            $fp = fopen($DB_Table_Option.".xml", "w") or die ("Unable to open file!");
+            
+            fwrite($fp, '<?xml version="1.0" encoding="utf-8" ?>'.PHP_EOL);
+           
+            fwrite($fp, '<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"'.PHP_EOL);
+            fwrite($fp, "\t\n".'xmlns:archi="http://architecture/'.$DB_Table_Option.'">'.PHP_EOL);  
+            fwrite($fp, PHP_EOL);
+            
+            $i = 0;
+            
+            
+            while($this->row = $data->fetch_assoc()){    
+               
+                fwrite($fp, "\t".'<rdf:Description rdf:about="http://architecture/'.$DB_Table_Option.'">'.PHP_EOL);
+                foreach($colnames as $colname){  
+                    
+                    fwrite($fp, "\t\t".'<archi:'.$colname.'>'.$this->row[$colname].'</archi:'.$colname.'>'.PHP_EOL);
+                    
+                }
+                fwrite($fp, "\t".'</rdf:Description>'.PHP_EOL);
+                
+            }
+
+          
+            fwrite($fp, '</rdf:RDF>');
+            printf($DB_Table_Option.'.xml create complete'."\n");
+
+            
+        }
+    
+    
+        public function select_DB($DB_Table_Option) {
+    
+            $this->colname = array();
+            
+            $this->colname = $this->get_column_name($DB_Table_Option); //get table column name
+            
+            $this->sql = "SELECT * from $DB_Table_Option"; 
+            $this->result = $this->conn->query($this->sql);
+    
+            if(!$this->result){
+                
+                if(!mysqli_ping($this->conn)){
+                    $this->reconnect();
+                    $this->update_DB();
+                }
+                else if($this->rows->num_rows == 0){
+                    echo "0 results\n";
+                    exit;
+                }
+                else{
+                    echo "SQL Error: " . mysqli_error($this->conn)."\n";
+                    exit;
                 }
             }
-            return null;
+            else{
+                $this->create_xml($DB_Table_Option, $this->result, $this->colname);
+   
+            }
+           
         }
 
-        function age()
-        {
-            $birth = $this->birthEvent();
-            if ($birth) {
-                $year = substr($birth->get('bio:date'), 0, 4);
-                if ($year) {
-                    return date('Y') - $year;
+        public function get_column_name($DB_Table_Option){
+            
+            $this->sql = "DESCRIBE $DB_Table_Option";
+            $this->result = $this->conn->query($this->sql);
+            $this->colname = array();
+
+            if(!$this->result){
+                
+                if(!mysqli_ping($this->conn)){
+                    $this->reconnect();
+                    $this->update_DB();
+                }
+                else if($this->rows->num_rows == 0){
+                    echo "0 results\n";
+                    exit;
+                }
+                else{
+                    echo "SQL Error: " . mysqli_error($this->conn)."\n";
+                    exit;
                 }
             }
-            return 'unknown';
+            else{
+                while($this->row = $this->result->fetch_assoc()){
+                    //printf($this->row['Field']."\n");
+                    array_push($this->colname,$this->row['Field']);
+                }
+            }
+
+            return $this->colname;
+            //print_r($this->colname);
+
         }
+
+        
+    
     }
 
-    ## Add namespaces
-    EasyRdf_Namespace::set('mo', 'http://purl.org/ontology/mo/');
-    EasyRdf_Namespace::set('bio', 'http://purl.org/vocab/bio/0.1/');
-    EasyRdf_TypeMapper::set('mo:MusicArtist', 'Model_MusicArtist');
+    function read_chioce(){
+    
+        $fp1=fopen("php://stdin", "r");
+        $input=fgets($fp1, 255);
+        fclose($fp1);
+    
+        return $input;
+    }
+
+
 ?>
-<html>
-<head><title>EasyRdf Artist Info Example</title></head>
-<body>
-<h1>EasyRdf Artist Info Example</h1>
-
-<?= form_tag() ?>
-<?= text_field_tag('uri', 'http://www.bbc.co.uk/music/artists/70248960-cb53-4ea4-943a-edb18f7d336f.rdf', array('size'=>50)) ?>
-<?= submit_tag() ?>
-<?= form_end_tag() ?>
-
-<?php
-    if (isset($_REQUEST['uri'])) {
-        $graph = EasyRdf_Graph::newAndLoad($_REQUEST['uri']);
-        $artist = $graph->primaryTopic();
-    }
-
-    if (isset($artist)) {
-?>
-
-<dl>
-    <dt>Artist Name:</dt><dd><?= $artist->get('foaf:name') ?></dd>
-    <dt>Type:</dt><dd><?= join(', ', $artist->types()) ?></dd>
-    <dt>Homepage:</dt><dd><?= link_to($artist->get('foaf:homepage')) ?></dd>
-    <dt>Wikipedia page:</dt><dd><?= link_to($artist->get('mo:wikipedia')) ?></dd>
-    <?php
-        if ($artist->isA('mo:SoloMusicArtist')) {
-            echo "  <dt>Age:</dt>";
-            echo "  <dd>".$artist->age()."</dd>\n";
-        }
-    ?>
-</dl>
-<?php
-    }
-
-    if (isset($graph)) {
-        echo $graph->dump();
-    }
-?>
-</body>
-</html>
